34c28a863fa830983dde6e9e470e78f615470716
Vanya <vanya.kulig@yandex.ru>
2025-01-15 06:11:54 +0300

    Лабораторная работа 6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.querySelectorAll('.header__menu-button,.header__close-button').forEach((button) => {
-    button.addEventListener("click", () => {
-      document.querySelectorAll('.header__menu-button,.header__close-button,.header__navigation,.header__address,.menu__mail').forEach((element) => {
-        element.classList.toggle('hidden');
-      });
-  
-      document.querySelector('.header__title').classList.toggle('menu-header__title');
-  
-      document.querySelector('.header__container').classList.toggle('show-menu');
-    })
+  button.addEventListener("click", () => {
+    document.querySelectorAll('.header__menu-button,.header__close-button,.header__navigation,.header__address,.menu__mail').forEach((element) => {
+      element.classList.toggle('hidden');
+    });
+
+    document.querySelector('.header__title').classList.toggle('menu-header__title');
+
+    document.querySelector('.header__container').classList.toggle('show-menu');
   })
+})
